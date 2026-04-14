86ebcfa36d694432ab59cda4b98b4bcce6383d29
Yosaire <96118628+Yosaire@users.noreply.github.com>
2026-04-14 16:25:46 +0200

    Adding componets

---
+++ src/App.vue
@@ -0,0 +1,19 @@
+<script setup>
+import Header from "./components/Header.vue";
+import Balance from "./components/Balance.vue";
+import IncomeExpenses from "./components/IncomeExpenses.vue";
+import TransactionList from "./components/TransactionList.vue";
+import AddTransaction from "./components/AddTransaction.vue";
+</script>
+
+<template>
+  <Header />
+  <div class="container">
+    <AddTransaction />
+    <Balance />
+    <IncomeExpenses />
+    <TransactionList />
+  </div>
+</template>
+
+<style scoped></style>
---
+++ src/components/IncomeExpenses.vue
@@ -0,0 +1,12 @@
+<template>
+  <div class="inc-exp-container">
+    <div>
+      <h4>Income</h4>
+      <p id="money-plus" class="money plus">+0.00 zł</p>
+    </div>
+    <div>
+      <h4>Expenses</h4>
+      <p id="money-minus" class="money minus">-0.00 zł</p>
+    </div>
+  </div>
+</template>
